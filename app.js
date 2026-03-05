d2360e51cab70820328ca52e3cb653482aa1f2bb
E-LEARN CORRIDOR <kavumarodgersscientist@gmail.com>
2026-03-05 22:25:00 +0300

    Refactor content structure and add timetable display
    Refactor content structure and add functionality to display timetable for each subject. Implement event listeners for buttons to show respective content.

--- app.js
+++ app.js
@@ -12,34 +12,34 @@ let chemMathContent = {
     resources: ["Chemistry textbooks", "Mathematics textbooks", "Online tutorials", "Practice problems", "Interactive simulations"],
     timetable: {
         monday: [
-            {time: "8:00AM-10:00AM", course: "None", lecturer: "None", code: "None"},
+            {time: "08:00AM-10:00AM", course: "None", lecturer: "None", code: "None"},
             {time: "10:00AM-12:00PM", course: "None", lecturer: "None", code: "None"},
-            {time: "12:00PM-2:00PM", course: "None", lecturer: "None", code: "None"},
-            {time: "2:00PM-4:00PM", course: "Analytical and Aplied Chemistry", lecturer: "Dr. XXXXX", code: "SED 2202"}
+            {time: "12:00PM-02:00PM", course: "None", lecturer: "None", code: "None"},
+            {time: "02:00PM-04:00PM", course: "Analytical and Aplied Chemistry", lecturer: "Dr. XXXXX", code: "SED 2202"}
         ],
         tuesday: [
-            {time: "8:00AM-10:00AM", course: "Numerical Analysis", lecturer: "Dr. Savannah", code: "SMT 221"},
+            {time: "08:00AM-10:00AM", course: "Numerical Analysis", lecturer: "Dr. Savannah", code: "SMT 221"},
             {time: "10:00AM-12:00PM", course: "Probability Theory", lecturer: "Dr. Hasifah", code: "SMT 222"},
-            {time: "12:00PM-2:00PM", course: "General methods, education technology and school prsctice", lecturer: "Dr. Bashir", code: "EPS 2201"},
-            {time: "2:00PM-4:00PM", course: "Kinetics and Electrochemistry", lecturer: "Dr. YYYYY", code: "SED 2201"}
+            {time: "12:00PM-02:00PM", course: "General methods, education technology and school prsctice", lecturer: "Dr. Bashir", code: "EPS 2201"},
+            {time: "02:00PM-04:00PM", course: "Kinetics and Electrochemistry", lecturer: "Dr. YYYYY", code: "SED 2201"}
         ],
         wednesday: [
-            {time: "8:00AM-10:00AM", course: "Probability Theory", lecturer: "Dr. Hasifah", code: "SMT 222"},
+            {time: "08:00AM-10:00AM", course: "Probability Theory", lecturer: "Dr. Hasifah", code: "SMT 222"},
             {time: "10:00AM-12:00PM", course: "Mathematics Education", lecturer: "Dr. Jalilah", code: "SMT 223"},
-            {time: "12:00PM-2:00PM", course: "General methods, education technology and school prsctice", lecturer: "Dr. Bashir", code: "EPS 2201"},
-            {time: "2:00PM-4:00PM", course: "Numerical Analysis", lecturer: "Dr. Savannah", code: "SMT 221"}
+            {time: "12:00PM-02:00PM", course: "General methods, education technology and school prsctice", lecturer: "Dr. Bashir", code: "EPS 2201"},
+            {time: "02:00PM-04:00PM", course: "Numerical Analysis", lecturer: "Dr. Savannah", code: "SMT 221"}
         ],
         thursday: [
-            {time: "8:00AM-10:00AM", course: "Chemistry methods and Research methods", lecturer: "Dr. Nanyonga", code: "SED 2203"},
+            {time: "08:00AM-10:00AM", course: "Chemistry methods and Research methods", lecturer: "Dr. Nanyonga", code: "SED 2203"},
             {time: "10:00AM-12:00PM", course: "Mathematics Education", lecturer: "Dr. Jalilah", code: "SMT 223"},
-            {time: "12:00PM-2:00PM", course: "Learning measurements and Evaluation", lecturer: "Ass. Professor Joseph", code: "EPS 2202"},
-            {time: "2:00PM-4:00PM", course: "None", lecturer: "None", code: "None"}
+            {time: "12:00PM-02:00PM", course: "Learning measurements and Evaluation", lecturer: "Ass. Professor Joseph", code: "EPS 2202"},
+            {time: "02:00PM-04:00PM", course: "None", lecturer: "None", code: "None"}
         ],
         friday: [
-            {time: "8:00AM-10:00AM", course: "None", lecturer: "None", code: "None"},
+            {time: "08:00AM-10:00AM", course: "None", lecturer: "None", code: "None"},
             {time: "10:00AM-12:00PM", course: "None", lecturer: "None", code: "None"},
-            {time: "12:00PM-2:00PM", course:"Learning Measurement and Evaluation", lecturer: "Assoc Prof Joseph Ssenyonga", code: "EPS 2202"},
-            {time: "2:00PM-4:00PM", course: "None", lecturer: "None", code: "None"}
+            {time: "12:00PM-02:00PM", course:"Learning Measurement and Evaluation", lecturer: "Assoc Prof Joseph Ssenyonga", code: "EPS 2202"},
+            {time: "02:00PM-04:00PM", course: "None", lecturer: "None", code: "None"}
         ]
     }
 };
@@ -51,34 +51,34 @@ let phyMathContent = {
     resources: ["Physics textbooks", "Mathematics textbooks", "Online tutorials", "Practice problems", "Interactive simulations"],
     timetable: {
         monday: [
-            {time: "8:00AM-10:00AM", course: "Digital Electronics", lecturer: "Dr. Okullo Michael", code: "SPH 2203"},
+            {time: "08:00AM-10:00AM", course: "Digital Electronics", lecturer: "Dr. Okullo Michael", code: "SPH 2203"},
             {time: "10:00AM-12:00PM", course: "Nuclear Physics", lecturer: "Dr. William Ochen", code: "SPH 2204"},
-            {time: "12:00PM-2:00PM", course: "Physics Education II", lecturer: "Dr. Joyce Mbambu", code: "SPH 2201"},
-            {time: "2:00PM-4:00PM", course: "Physics Education II", lecturer: "Dr. Joyce Mbambu", code: "SPH 2201"}
+            {time: "12:00PM-02:00PM", course: "Physics Education II", lecturer: "Dr. Joyce Mbambu", code: "SPH 2201"},
+            {time: "02:00PM-04:00PM", course: "Physics Education II", lecturer: "Dr. Joyce Mbambu", code: "SPH 2201"}
         ],
         tuesday: [
-            {time: "8:00AM-10:00AM", course:"Numerical Analysis I", lecturer: "Dr. Nuwagaba Savannah", code: "SMT 221"},
+            {time: "08:00AM-10:00AM", course:"Numerical Analysis I", lecturer: "Dr. Nuwagaba Savannah", code: "SMT 221"},
             {time: "10:00AM-12:00PM", course:"Probability Theory", lecturer: "Dr. Nampala Hasifah", code: "SMT 222"},
-            {time: "12:00PM-2:00PM", course:"General Methods, Educational Technology and School Practice", lecturer: "Dr. Kishabale Bashir", code: "EPS 2201"},
-            {time: "2:00PM-4:00PM", course:"Digital Electronics", lecturer: "Dr. Okullo Michael", code: "SPH 2203"}
+            {time: "12:00PM-02:00PM", course:"General Methods, Educational Technology and School Practice", lecturer: "Dr. Kishabale Bashir", code: "EPS 2201"},
+            {time: "02:00PM-04:00PM", course:"Digital Electronics", lecturer: "Dr. Okullo Michael", code: "SPH 2203"}
         ],
         wednesday: [
-            {time: "8:00AM-10:00AM", course:"Probability Theory", lecturer: "Dr. Nampala Hasifah", code: "SMT 222"},
+            {time: "08:00AM-10:00AM", course:"Probability Theory", lecturer: "Dr. Nampala Hasifah", code: "SMT 222"},
             {time: "10:00AM-12:00PM", course:"Mathematics Education", lecturer: "Dr. Namuwaya Jalilah", code: "SMT 223"},
-            {time: "12:00PM-2:00PM", course:"General Methods, Educational Technology and School Practice", lecturer: "Dr. Kishabale Bashir", code: "EPS 2201"},
-            {time: "2:00PM-4:00PM", course:"Numerical Analysis I", lecturer: "Dr. Nuwagaba Savannah", code: "SMT 221"}
+            {time: "12:00PM-02:00PM", course:"General Methods, Educational Technology and School Practice", lecturer: "Dr. Kishabale Bashir", code: "EPS 2201"},
+            {time: "02:00PM-04:00PM", course:"Numerical Analysis I", lecturer: "Dr. Nuwagaba Savannah", code: "SMT 221"}
         ],
         thursday: [
-            {time: "8:00AM-10:00AM", course:"Electromagnetism", lecturer: "Dr. Bakulu Thomas", code: "SPH 2202"},
+            {time: "08:00AM-10:00AM", course:"Electromagnetism", lecturer: "Dr. Bakulu Thomas", code: "SPH 2202"},
             {time: "10:00AM-12:00PM", course:"Mathematics Education", lecturer: "Dr. Namuwaya Jalilah", code: "SMT 223"},
-            {time: "12:00PM-2:00PM", course:"Learning Measurement and Evaluation", lecturer: "Assoc Prof Joseph Ssenyonga", code: "EPS 2202"},
-            {time: "2:00PM-4:00PM", course:"Nuclear Physics", lecturer: "Dr. William Ochen", code: "SPH 2204"}
+            {time: "12:00PM-02:00PM", course:"Learning Measurement and Evaluation", lecturer: "Assoc Prof Joseph Ssenyonga", code: "EPS 2202"},
+            {time: "02:00PM-04:00PM", course:"Nuclear Physics", lecturer: "Dr. William Ochen", code: "SPH 2204"}
         ],
         friday: [
-            {time: "8:00AM-10:00AM", course:"Electromagnetism", lecturer: "Dr. Bakulu Thomas", code: "SPH 2202"},
+            {time: "08:00AM-10:00AM", course:"Electromagnetism", lecturer: "Dr. Bakulu Thomas", code: "SPH 2202"},
             {time: "10:00AM-12:00PM", course:"None", lecturer: "None", code: "None"},
-            {time: "12:00PM-2:00PM", course:"Learning Measurement and Evaluation", lecturer: "Assoc Prof Joseph Ssenyonga", code: "EPS 2202"},
-            {time: "2:00PM-4:00PM", course:"PRACTICALS", lecturer: "xxxxx", code: "xxxxx"}
+            {time: "12:00PM-02:00PM", course:"Learning Measurement and Evaluation", lecturer: "Assoc Prof Joseph Ssenyonga", code: "EPS 2202"},
+            {time: "02:00PM-04:00PM", course:"PRACTICALS", lecturer: "xxxxx", code: "xxxxx"}
         ]
         }
 };
@@ -90,34 +90,34 @@ let bioChemContent = {
     resources: ["Biology textbooks", "Chemistry textbooks", "Online tutorials", "Practice problems", "Interactive simulations"],
     timetable: {
         monday: [
-            {time: "8:00AM-10:00AM", course:"Human Ecology", lecturer: "xxxxx", code: "SBE 2201"},
+            {time: "08:00AM-10:00AM", course:"Human Ecology", lecturer: "xxxxx", code: "SBE 2201"},
             {time: "10:00AM-12:00PM", course:"Evolution", lecturer: "xxxxx", code: "SBE 2203"},
-            {time: "12:00PM-2:00PM", course:"Evolution", lecturer: "xxxxx", code: "SBE 2203"},
-            {time: "2:00PM-4:00PM", course:"Analytical and Applied Chemistry", lecturer: "xxxxx", code: "SED 2202"}
+            {time: "12:00PM-02:00PM", course:"Evolution", lecturer: "xxxxx", code: "SBE 2203"},
+            {time: "02:00PM-04:00PM", course:"Analytical and Applied Chemistry", lecturer: "xxxxx", code: "SED 2202"}
         ],
         tuesday: [
-            {time: "8:00AM-10:00AM", course:"Genetics", lecturer: "xxxxx", code: "SBE 2202"},
+            {time: "08:00AM-10:00AM", course:"Genetics", lecturer: "xxxxx", code: "SBE 2202"},
             {time: "10:00AM-12:00PM", course:"Genetics", lecturer: "xxxxx", code: "SBE 2202"},
-            {time: "12:00PM-2:00PM", course:"General Methods, Educational Technology and School Practice", lecturer: "Dr. Kishabale Bashir", code: "EPS 2201"},
-            {time: "2:00PM-4:00PM", course:"Kinetics and Electrochemistry II", lecturer: "xxxxx", code: "SED 2201"}
+            {time: "12:00PM-02:00PM", course:"General Methods, Educational Technology and School Practice", lecturer: "Dr. Kishabale Bashir", code: "EPS 2201"},
+            {time: "02:00PM-04:00PM", course:"Kinetics and Electrochemistry II", lecturer: "xxxxx", code: "SED 2201"}
         ],
         wednesday: [
-            {time: "8:00AM-10:00AM", course:"Biology Education", lecturer: "xxxxx", code: "SBE 2204"},
+            {time: "08:00AM-10:00AM", course:"Biology Education", lecturer: "xxxxx", code: "SBE 2204"},
             {time: "10:00AM-12:00PM", course:"Biology Education", lecturer: "xxxxx", code: "SBE 2204"},
-            {time: "12:00PM-2:00PM", course:"General Methods, Educational Technology and School Practice", lecturer: "Dr. Kishabale Bashir", code: "EPS 2201"},
-            {time: "2:00PM-4:00PM", course:"Animal Physiology", lecturer: "xxxxx", code: "SBE 2205"}
+            {time: "12:00PM-02:00PM", course:"General Methods, Educational Technology and School Practice", lecturer: "Dr. Kishabale Bashir", code: "EPS 2201"},
+            {time: "02:00PM-04:00PM", course:"Animal Physiology", lecturer: "xxxxx", code: "SBE 2205"}
         ],
         thursday: [
-            {time: "8:00AM-10:00AM", course:"Chemistry Methods I and Research Methods", lecturer: "Dr. Nanyonga", code: "SED 2203"},
+            {time: "08:00AM-10:00AM", course:"Chemistry Methods I and Research Methods", lecturer: "Dr. Nanyonga", code: "SED 2203"},
             {time: "10:00AM-12:00PM", course:"None", lecturer: "None", code: "None"},
-            {time: "12:00PM-2:00PM", course:"Learning Measurement and Evaluation", lecturer: "Assoc Prof Joseph Ssenyonga", code: "EPS 2202"},
-            {time: "2:00PM-4:00PM", course:"None", lecturer: "None", code: "None"}
+            {time: "12:00PM-02:00PM", course:"Learning Measurement and Evaluation", lecturer: "Assoc Prof Joseph Ssenyonga", code: "EPS 2202"},
+            {time: "02:00PM-04:00PM", course:"None", lecturer: "None", code: "None"}
         ],
         friday: [
-            {time: "8:00AM-10:00AM", course:"None", lecturer: "None", code: "None"},
+            {time: "08:00AM-10:00AM", course:"None", lecturer: "None", code: "None"},
             {time: "10:00AM-12:00PM", course:"None", lecturer: "None", code: "None"},
-            {time: "12:00PM-2:00PM", course:"Learning Measurement and Evaluation", lecturer: "Assoc Prof Joseph Ssenyonga", code: "EPS 2202"},
-            {time: "2:00PM-4:00PM", course:"None", lecturer: "None", code: "None"}
+            {time: "12:00PM-02:00PM", course:"Learning Measurement and Evaluation", lecturer: "Assoc Prof Joseph Ssenyonga", code: "EPS 2202"},
+            {time: "02:00PM-04:00PM", course:"None", lecturer: "None", code: "None"}
         ]
         }
 };
@@ -134,17 +134,36 @@ function showProgramme(data) {
     <ul>
     ${data.resources.map(resource=>`<li>${resource}</li>`).join('')}
     </ul>
-    <h3>Monday</h3>
-    ${buildDayTable(data.timetable.monday)}
-    <h3>Tuesday</h3>
-    ${buildDayTable(data.timetable.tuesday)}
-    <h3>Wednesday</h3>
-    ${buildDayTable(data.timetable.wednesday)}
-    <h3>Thursday</h3>
-    ${buildDayTable(data.timetable.thursday)}
-    <h3>Friday</h3>
-    ${buildDayTable(data.timetable.friday)}`;
+    <div class="tab-buttons">
+        <button onclick="showDay('monday')">Monday</button>
+        <button onclick="showDay('tuesday')">Tuesday</button>
+        <button onclick="showDay('wednesday')">Wednesday</button>
+        <button onclick="showDay('thursday')">Thursday</button>
+        <button onclick="showDay('friday')">Friday</button>
+    </div>
+    <div id="day-monday">
+        <h3>Monday</h3>
+        ${buildDayTable(data.timetable.monday)}
+    </div>
+    <div id="day-tuesday">
+        <h3>Tuesday</h3>
+        ${buildDayTable(data.timetable.tuesday)}
+    </div>
+    <div id="day-wednesday">
+        <h3>Wednesday</h3>
+        ${buildDayTable(data.timetable.wednesday)}
+    </div>
+    <div id="day-thursday">
+        <h3>Thursday</h3>
+        ${buildDayTable(data.timetable.thursday)}
+    </div>
+    <div id="day-friday">
+        <h3>Friday</h3>
+        ${buildDayTable(data.timetable.friday)}
+    </div>
+}`;
 }
+//Function to build the timetable for each day
 function buildDayTable(slots) {
     return `
     <table>
@@ -179,3 +198,15 @@ phyMathBtn.addEventListener('click', function() {
 bioChemBtn.addEventListener('click', function() {
     showProgramme(bioChemContent)
 });
+
+//Function to show the timetable for each day and hide the others
+function showDay(day) {
+    const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+    for (let i=0; i<days.length; i++) {
+        if (days[i] === day) {
+            document.getElementById(`day-${day}`).style.display = "block";
+        } else {
+            document.getElementById(`day-${days[i]}`).style.display = "none";
+        }
+    }
+}
